305949863ea924bc0e6469717cb87e2f123e5459
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-03-25 14:44:11 +0100

    omdb

--- src/app/omdb/omdb.page.ts
+++ src/app/omdb/omdb.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-omdb',
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./omdb.page.scss'],
 })
 export class OmdbPage implements OnInit {
+  omdbContent: object[] = [];
+  omdbUrl: string = 'http://www.omdbapi.com/?t=tarzan&apikey=9a892ca8';
 
-  constructor() { }
+
+  constructor(private http: HttpClient) {
+    this.http.get(this.omdbUrl, { responseType: 'json' }).subscribe((data: any[]) => {
+      console.log(data);
+      this.omdbContent = data;
+    });
+
+  }
 
   ngOnInit() {
   }
